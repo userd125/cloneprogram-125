1b6772727ba70c348571477e03d5dccaedce70af
userd121 <2394271992@qq.com>
2020-10-07 08:28:46 +0800

    caculate.cs is my.

--- caculate.cs
+++ caculate.cs
@@ -1,1 +1,15 @@
-helloword
+public static class caculate
+{
+	public int add(int a,int b)
+	{
+		return a+b;
+	}
+	public int max(int a,int b)
+	{
+		if(a>b)
+			return a;
+		else
+			return b;
+	}
+}
+
